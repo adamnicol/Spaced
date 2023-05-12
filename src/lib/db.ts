d40dbd9d15964913849dbd8abaa4076b63e58923
Adam Nicol <adam.nicol@protonmail.com>
2023-05-12 21:27:55 +0100

    Limit prisma to one instance in dev

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-const db = new PrismaClient();
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    //log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
 
 export default db;
